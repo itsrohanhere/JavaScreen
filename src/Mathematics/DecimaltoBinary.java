48bf232582e496606586d0fef94f4be2ed2e0fda
itsrohanhere <rohankha332@gmail.com>
2025-12-10 18:30:53 +0530

    added sum of first N number

--- src/Mathematics/DecimaltoBinary.java
+++ src/Mathematics/DecimaltoBinary.java
@@ -2,8 +2,9 @@ package Mathematics;
 
 public class DecimaltoBinary {
     public static void main(String[] args) {
-        System.out.println(Integer.toBinaryString(253));
-        System.out.println(decimalBinary(1632));
+//        System.out.println(Integer.toBinaryString(253));
+//        System.out.println(decimalBinary(1632));
+        binaryPalindrome(91);
     }
     static String decimalBinary(int decimalNum){
         StringBuilder sb = new StringBuilder();
@@ -13,6 +14,16 @@ public class DecimaltoBinary {
             decimalNum = decimalNum/2;
         }
         return sb.reverse().toString();
+    }
+
+    static void binaryPalindrome(int num){
+        StringBuilder sb = new StringBuilder(decimalBinary(num));
+        StringBuilder sb2 = new StringBuilder(sb.reverse().toString());
+//        System.out.println(sb.reverse().toString());
+        String two = sb.toString();
+        String one = sb.reverse().toString();
+
+        System.out.println(one.equals(two));
     }
 }
 
---
+++ src/Mathematics/sumOfFirstN.java
@@ -0,0 +1,10 @@
+package Mathematics;
+
+public class sumOfFirstN {
+    public static void main(String[] args) {
+        int n = 10;
+        int sum = 0;
+        sum = (n+1)*(n/2);
+        System.out.println(sum);
+    }
+}

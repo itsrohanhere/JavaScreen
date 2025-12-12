8ae0d4d3a9cbe03104106fb86cde82e87b652815
itsrohanhere <rohankha332@gmail.com>
2025-12-12 15:54:53 +0530

    added program to unset the bit in the Toggle class

--- src/BitManipulation/Toggle.java
+++ src/BitManipulation/Toggle.java
@@ -1,6 +1,10 @@
 package BitManipulation;
 
 public class Toggle {
+    static void unset(int num, int bit){
+        int n = num & (~(1 << bit));
+        System.out.println(n);
+    }
     static void toggle(int n, int bit){
         int num = n ^ (1<<bit);
         System.out.println(num);
@@ -9,6 +13,7 @@ public class Toggle {
     public static void main(String[] args) {
         // toggle krna hai ji
         // 0 hai to 1 aur 1 hai to 0
-        toggle(35,5);
+        toggle(35,4);
+        unset(35,4);
     }
 }

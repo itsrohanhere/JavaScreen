d243b8efe5e3d0d2e3e06084771e74c6d5de40aa
itsrohanhere <rohankha332@gmail.com>
2025-12-12 16:12:51 +0530

    added program to check whether a number is power of two or not

--- src/BitManipulation/Toggle.java
+++ src/BitManipulation/Toggle.java
@@ -10,10 +10,19 @@ public class Toggle {
         System.out.println(num);
     }
 
+    static void powerOfTwo(int n){
+        if((n & (n-1))==0){
+            System.out.println(n+" is a power of two");
+        }else{
+            System.out.println(n+" is not a power of two");
+        }
+    }
+
     public static void main(String[] args) {
         // toggle krna hai ji
         // 0 hai to 1 aur 1 hai to 0
-        toggle(35,4);
-        unset(35,4);
+//        toggle(35,4);
+//        unset(35,4);
+        powerOfTwo(8);
     }
 }

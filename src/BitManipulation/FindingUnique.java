808a9cfbb1f1f770d2d7017d9567bbb4901dc3eb
itsrohanhere <rohankha332@gmail.com>
2025-12-13 12:41:02 +0530

    added program to find unique element in an array

---
+++ src/BitManipulation/FindingUnique.java
@@ -0,0 +1,15 @@
+package BitManipulation;
+
+public class FindingUnique {
+    public static void main(String[] args) {
+        // finding unique elements from an array
+        int[] arr = {5,6,7,4,5,6,7};
+        int a=0;
+        int index = 0;
+        while(index<arr.length){
+            a = a^arr[index];
+            index++;
+        }
+        System.out.println(a);
+    }
+}
